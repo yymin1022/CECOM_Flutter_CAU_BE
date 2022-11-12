44fb6093e2e576de519c59102aeacccaf9129e9b
yymin1022 <yymin1022@gmail.com>
2022-11-13 04:13:24 +0900

    Update app.ts : Added Base Code for Express Server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, World!");
+});
+
+app.listen(8080, () => {
+    console.log("Server is Listening on Port 8080!");
+});
