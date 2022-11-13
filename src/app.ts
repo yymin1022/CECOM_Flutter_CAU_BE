5d23cd38977bea866c883cd59fccc6e9a2cd4b01
yymin1022 <yymin1022@gmail.com>
2022-11-13 17:26:37 +0900

    Update app.ts : Added getMeal API Function

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
 });
 
+app.post("/getMeal", (req: Request, res: Response) => {
+    res.send("Hello, World! Meal Data!");
+})
+
 app.listen(8080, () => {
     console.log("Server is Listening on Port 8080!");
 });
