81255373e868abd67b73778eeb03068e87928d98
yymin1022 <yymin1022@gmail.com>
2022-12-31 02:04:31 +0900

    Update DataType : Added Basic Data Interfaces

--- src/Util/DataType.ts
+++ src/Util/DataType.ts
@@ -1,5 +1,21 @@
-export interface API_DATA{
+export interface API_DATA {
     RESULT_CODE: number,
     RESULT_MSG: string,
-    RESULT_DATA: object
+    RESULT_DATA: LIBRARY_DATA | MEAL_DATA | NOTICE_DATA | SCHEDULE_DATA
 }
+
+export interface LIBRARY_DATA {
+
+};
+
+export interface MEAL_DATA {
+
+};
+
+export interface NOTICE_DATA {
+
+};
+
+export interface SCHEDULE_DATA {
+
+};
